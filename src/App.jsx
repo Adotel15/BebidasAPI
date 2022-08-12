2bd6b22b1b78d3be74419d6e710e6f86be08718f
Adotel <adriandotel@gmail.com>
2022-08-12 13:48:06 +0200

    Creado el Context, y el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import { Container } from 'react-bootstrap'
 import Fomulario from './components/Fomulario'
+import { CategoriasProvider } from './context/CategoriasProvider'
 
 function App() {
 
   return (
-    <>
+    <CategoriasProvider>
       <header className = "py-5">
         <h1>Buscador de Bebidas</h1>
       </header>
@@ -13,7 +14,7 @@ function App() {
       <Container className = 'mt-5'>
         <Fomulario />
       </Container>
-    </>
+    </CategoriasProvider>
   )
 }
 
--- src/components/Fomulario.jsx
+++ src/components/Fomulario.jsx
@@ -7,10 +7,28 @@ const Fomulario = () => {
         <Form>
             <Row>
                 <Col md = {6}>
-                    1
+                    <Form.Group className = 'mb-3'>
+                        <Form.Label htmlFor = 'nombre'>Nombre de la bebida</Form.Label>
+
+                        <Form.Control 
+                            type = "text"
+                            placeholder = 'Ej: Tequila, Vodka...'
+                            name = "nombre"
+                        />
+                    </Form.Group>
                 </Col>
+
                 <Col md = {6}>
-                    2
+                <Form.Group className = 'mb-3'>
+                        <Form.Label htmlFor = 'categoria'>Categoria Bebida</Form.Label>
+
+                        <Form.Select
+                            id = 'categoria'
+                            name = 'categoria'
+                        >
+                            <option>- Seleciona Categoria -</option>
+                        </Form.Select>
+                    </Form.Group>
                 </Col>
             </Row>
         </Form>
